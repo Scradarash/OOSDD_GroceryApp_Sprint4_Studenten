2bacb206cad9706438154a97a6bf85faf46ee672
Kaj <s1209000@student.windesheim.nl>
2025-10-03 09:18:13 +0200

    Productnaam nu zichtbaar in dropdown van producten in de gekochte producten tab. Ook de label die voor de tekst van de grid staat naar beneden verplaatst. Simpele unit tests geschreven die controleert wat er gebeurt met een positief, negatief en precies 0 product amount.

--- TestCore/TestHelpers.cs
+++ TestCore/TestHelpers.cs
@@ -1,4 +1,5 @@
 using Grocery.Core.Helpers;
+using Grocery.Core.Models;
 
 namespace TestCore
 {
@@ -31,14 +32,47 @@ namespace TestCore
         [Test]
         public void TestPasswordHelperReturnsFalse()
         {
-            Assert.Pass(); //Zelf uitwerken
+            string password = "user3";
+            string passwordHash = "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA1";
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
         }
 
-        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM08")]
-        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA")]
+        [TestCase("user1", "IunRhDKa+fWo8+4/Qfj7Pg==.kDxZnUQHCZun6gLIE6d9oeULLRIuRmxmH2QKJv2IM081")]
+        [TestCase("user3", "sxnIcZdYt8wC8MYWcQVQjQ==.FKd5Z/jwxPv3a63lX+uvQ0+P7EuNYZybvkmdhbnkIHA1")]
         public void TestPasswordHelperReturnsFalse(string password, string passwordHash)
         {
-            Assert.Fail(); //Zelf uitwerken zodat de test slaagt!
+            Assert.IsFalse(PasswordHelper.VerifyPassword(password, passwordHash));
+        }
+    }
+
+    public class TestGroceryListItem
+    {
+        [Test]
+        public void GroceryListItem_CreatesWithValidAmount()
+        {
+            var item = new GroceryListItem(1, 1, 1, 5);
+
+            Assert.That(item.Amount, Is.EqualTo(5));
+            Assert.That(item.ProductId, Is.EqualTo(1));
+            Assert.That(item.GroceryListId, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GroceryListItem_AllowsZeroAmount_CurrentBehavior()
+        {
+            var item = new GroceryListItem(1, 1, 1, 0);
+
+            // huidige gedrag: Amount mag 0 zijn
+            Assert.That(item.Amount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GroceryListItem_AllowsNegativeAmount_CurrentBehavior()
+        {
+            var item = new GroceryListItem(1, 1, 1, -3);
+
+            // huidige gedrag: negatieve waardes zijn toegestaan
+            Assert.That(item.Amount, Is.EqualTo(-3));
         }
     }
 }
